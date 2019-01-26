63897f8dacaa3d348845c4339ec06ffa11d2c3a5
Jane Venter <janeanneventer@gmail.com>
2019-01-26 19:25:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,8 +7,8 @@ class Pokemon
   
   def initialize(id = nil, name = nil, type = nil, db = nil)
     @id = id
-    @name = name,
-    @type = type,
+    @name = name
+    @type = type
     @db = db
     @@all << self
   end
