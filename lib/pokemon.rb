3783cb355d34425538b5472ba5fefa537278c32a
Jane Venter <janeanneventer@gmail.com>
2019-01-26 17:59:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,32 @@
+require 'pry'
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  @@all = []
+  
+  def initialize(id: 1, name: "Pikachu", type: "electric", db: "<SQLite3::Database:0x00000003a2c620")
+    @id = id
+    @name = name,
+    @type = type,
+    @db = db
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def self.find(id, db)
+    attr = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
+    # binding.pry
+    attr.flatten.each do |i|
+      
+    Pokemon.new(attr.flatten.join(", "))
+  end
+  
 end
