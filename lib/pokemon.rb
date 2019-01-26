2ecd55c14aaaf590174134dc7dcd14bdbc0f4803
Jane Venter <janeanneventer@gmail.com>
2019-01-26 18:37:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,7 +25,7 @@ class Pokemon
     attr = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
     a = attr.flatten[0]
     binding.pry
-   Pokemon.new(id: attr.flatten[0], name: attr.flatten[1], type: attr.flatten[2])
+   Pokemon.new(id: attr.flatten[0], name: attr.flatten[1][0], type: attr.flatten[2])
    
   end
   
