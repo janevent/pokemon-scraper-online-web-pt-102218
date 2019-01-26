9ac16506dc53b998a0ddbb39102b291b0512d951
Jane Venter <janeanneventer@gmail.com>
2019-01-26 18:30:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,6 +23,7 @@ class Pokemon
   
   def self.find(id, db)
     attr = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
+    a = attr.flatten[0]
     binding.pry
    Pokemon.new(id: attr.flatten[0], name: attr.flatten[1], type: attr.flatten[2])
    
