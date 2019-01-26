b580e2113902202ad4e5fe31bd0be0edfc1ca3a7
Jane Venter <janeanneventer@gmail.com>
2019-01-26 18:16:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,7 +23,8 @@ class Pokemon
   
   def self.find(id, db)
     attr = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)
-   "Pikachu" = Pokemon.new(id: attr.flatten[0], name: attr.flatten[1], type: attr.flatten[2])
+   Pokemon.new(id: attr.flatten[0], name: attr.flatten[1], type: attr.flatten[2])
+   attr.flatten[0]
   end
   
 end
